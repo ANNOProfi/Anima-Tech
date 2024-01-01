18c45edcf360f2116f940cd454e0020998c88b66
ANNOProfi <ANNOProfi@t-online.de>
2024-01-01 12:50:29 +0100

    Basic Project setup

---
+++ Source/Main.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace AnimaTech
+{
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.Message("Anima Tech loaded successfully!");
+        }
+    }
+
+}
